6e35d9d88300a3c0223decde275cf071e899a7ce
YPAzevedo <yago.pereira.az@gmail.com>
2020-04-24 16:49:33 -0300

    Updates

---
+++ precache-manifest.0e95e7d73d762d7a4fb04978980e7cdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c055613cf8a889b8d84a6b8ff72296",
+    "url": "/githubexplorer/index.html"
+  },
+  {
+    "revision": "d73ade065ce9560b3ea6",
+    "url": "/githubexplorer/static/js/2.18a4d143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubexplorer/static/js/2.18a4d143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3407109ae83ecc3d2c58",
+    "url": "/githubexplorer/static/js/main.0e0cd149.chunk.js"
+  },
+  {
+    "revision": "60f48515cde624c3638d",
+    "url": "/githubexplorer/static/js/runtime-main.eea8c3b4.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/githubexplorer/static/media/githubBackground.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/githubexplorer/static/media/logoExplorer.04f6798f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4088085646f8743ba0b3d048da69d86.js"
+  "/githubexplorer/precache-manifest.0e95e7d73d762d7a4fb04978980e7cdf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/githubexplorer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
