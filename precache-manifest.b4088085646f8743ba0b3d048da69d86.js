2e72b0d868f7379321130a6c1aa950e29c2c47ee
YPAzevedo <yago.pereira.az@gmail.com>
2020-04-24 16:48:13 -0300

    Updates

---
+++ precache-manifest.b4088085646f8743ba0b3d048da69d86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4095484c4a4d9ec1a1bf906bd769a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc8423eeaaa1036e371",
+    "url": "/static/js/2.1b276430.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b276430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3407109ae83ecc3d2c58",
+    "url": "/static/js/main.0e0cd149.chunk.js"
+  },
+  {
+    "revision": "a2b448e3ee2ee73551c4",
+    "url": "/static/js/runtime-main.3b2b0529.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/githubBackground.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logoExplorer.04f6798f.svg"
+  }
+]);
